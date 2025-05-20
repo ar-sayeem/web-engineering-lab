2e94e0ea738b914aca01020626beddc122fa6bcd
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2025-05-20 16:48:30 +0600

    Create basic.php

---
+++ Lab-2/basic.php
@@ -0,0 +1,26 @@
+<?php
+    echo "this is php\n";
+    $x = 70;
+    $y = "the price is";
+    $z = $x + 30;
+    // single line comment
+    # single line comment
+    /* multi-line comment
+    comment
+    comment
+    */
+    echo "$y $z\n";
+    
+    if($x > $z){
+        echo "z is larger value\n";
+    }else{
+        echo "x is larger value";
+    }
+    
+    function MyFunc($t){
+        echo "\n";
+        echo ""
+    }
+    
+    
+?>
